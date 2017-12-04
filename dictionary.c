7a89c1665c276c8fe1d0d4af0e42d7fd7db1ba72
Mark Seda <mseda@udel.edu>
2017-12-04 14:17:52 -0500

    Added hashtable snippet from The C Programming Language to be modified to fit our needs.

---
+++ dictionary.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+struct nlist { /* table entry: */
+    struct nlist *next; /* next entry in chain */
+    char *name; /* word  */
+    int *occurances; /* number of occurances */
+};
+
+#define HASHSIZE 101
+static struct nlist *hashtab[HASHSIZE]; /* pointer table */
+
+/* hash: form hash value for string s */
+unsigned hash(char *s)
+{
+    unsigned hashval;
+    for (hashval = 0; *s != '\0'; s++)
+      hashval = *s + 31 * hashval;
+    return hashval % HASHSIZE;
+}
+
+/* lookup: look for s in hashtab */
+struct nlist *lookup(char *s)
+{
+    struct nlist *np;
+    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+        if (strcmp(s, np->name) == 0)
+          return np; /* found */
+    return NULL; /* not found */
+}
+
+char *strdup(char *);
+/* install: put (name, defn) in hashtab */
+struct nlist *install(char *name, char *defn)
+{
+    struct nlist *np;
+    unsigned hashval;
+    if ((np = lookup(name)) == NULL) { /* not found */
+        np = (struct nlist *) malloc(sizeof(*np));
+        if (np == NULL || (np->name = strdup(name)) == NULL)
+          return NULL;
+        hashval = hash(name);
+        np->next = hashtab[hashval];
+        hashtab[hashval] = np;
+    } else /* already there */
+        free((void *) np->occurances); /*free previous defn */
+    if ((np->occurances = strdup(defn)) == NULL)
+       return NULL;
+    return np;
+}
+
+char *strdup(char *s) /* make a duplicate of s */
+{
+    char *p;
+    p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
+    if (p != NULL)
+       strcpy(p, s);
+    return p;
+}
