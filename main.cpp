62d3a67ddd02b5bbef6ecbcd4f2416da7ba7a026
colbybanbury <cbanbury@udel.edu>
2017-12-06 12:30:17 -0500

    fixed error with deque

--- main.cpp
+++ main.cpp
@@ -14,9 +14,11 @@ void createMap(deque<string> sampleText){
 	//it pops the words off the front of the deque and adds them to a \
 	map if they don't already exist as a key. 
 	//It then adds the following word to thevector paired with that key
-	string currentWord = sampleText.pop_front();
+	string currentWord = sampleText.front();
+	sampleText.pop_front();
 	while(sampleText.size()>0){
-		string nextWord = sampleText.pop_front();
+		string nextWord = sampleText.front();
+		sampleText.pop_front();
 		table[currentWord].push_back(nextWord);
 		currentWord = nextWord;
 	}
