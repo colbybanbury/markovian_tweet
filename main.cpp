4e4c1cc77dcedbe75f827e7100d39786dd201b72
colbybanbury <cbanbury@udel.edu>
2017-12-06 13:14:24 -0500

    fixed rand issue

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+#include <stdio.h>     
+#include <stdlib.h> 
 #include <iostream>
 #include <string>
 #include <deque>
 #include <map>
 #include <vector>
+#include <time.h>
 
 using namespace std;
+
+
 
 map<string, vector<string> > table;
 
@@ -13,13 +18,29 @@ void createMap(deque<string> sampleText){
 	appear in the sample text
 	//it pops the words off the front of the deque and adds them to a \
 	map if they don't already exist as a key. 
-	//It then adds the following word to thevector paired with that key
+	//It then adds the following word to the vector paired with that key
 	string currentWord = sampleText.front();
 	sampleText.pop_front();
 	while(sampleText.size()>0){
-		string nextWord = sampleText.front();
-		sampleText.pop_front();
+		string nextWord = sampleText.front();//get the string at the front of the deque
+		sampleText.pop_front();//pop the first string
 		table[currentWord].push_back(nextWord);
 		currentWord = nextWord;
 	}
+}
+
+deque<string> generateOutput(int numWords){
+	srand (time(NULL));
+	deque<string> output;
+	string currentWord;
+	int i;
+	vector<string> nextWordVector = table["~start~"];
+	while(output.size()<numWords){
+		i = rand() % nextWordVector.size();
+		currentWord = nextWordVector[i];
+		if(currentWord == "~end~"){return output;}
+		output.push_front(currentWord);
+		nextWordVector = table[currentWord];
+	}
+	return output;
 }
